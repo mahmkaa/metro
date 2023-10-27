cf2eb24a9477fff5e5db9f92fab3a0488f314b18
Pavel <cubertx1@gmail.com>
2023-10-27 21:50:00 +0700

    Initial Commit

---
+++ metroTextGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  metroTextGame
+//
+//  Created by Павел Петров on 27.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
